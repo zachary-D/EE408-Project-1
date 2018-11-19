9302daf84eb3e18173cd6470ec6627d9940df7d0
wheatobw <wheatobw@clarkson.edu>
2018-11-19 15:44:19 -0500

    Did my stuff

--- app/src/main/java/domain/ee408proj1/MainActivity.java
+++ app/src/main/java/domain/ee408proj1/MainActivity.java
@@ -1,13 +1,48 @@
 package domain.ee408proj1;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView Description;
+    private TextView Score;
+    private Button startQuiz;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startQuiz = (Button) findViewById(R.id.startQuiz);
+
+        Description = (TextView) findViewById(R.id.Description);
+        Score = (TextView) findViewById(R.id.Score);
+
+
+        Score.setText("Score: ");
+
+        View.OnClickListener lDo = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Intent myIntent = new Intent( this, QuestionActivity.class );
+                //this.startActivity( myIntent );
+            }
+        };
+
+        startQuiz.setOnClickListener(lDo);
+
     }
+
 }
+
