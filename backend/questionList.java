8783ad9cf1c6a9e319c5ae702a0ba1adfaef6550
Unknown <zacharyd1@live.com>
2018-11-18 21:58:29 -0500

    Wrote two classes.  Committed for compilation on VM

--- backend/questionList.java
+++ backend/questionList.java
@@ -1,7 +1,34 @@
-
-
-class java
+class questionList
 {
-
-
-}; 
+	questionList()
+	{
+		numQuestions = 0;
+	}
+	
+	questionList(int _numQuestions, question[] _questions)
+	{
+		numQuestions = _numQuestions;
+		questions = new questions[numQuestions];
+		for(int i = 0; i < numQuestions; ++)
+		{
+			questions[i] = _questions[i];
+		}
+	}
+	
+private:
+	question[] questions;
+	int numQuestions;
+	
+public:
+	question getQuestion(int index)
+	{
+		//TODO: Input validation for 0 <= index < numQuestions
+		return questions[index];
+	}
+	
+	void shuffle()
+	{
+		//TODO: Shuffle the order of the questions, and call shuffle() on each one of them.
+		
+	}
+}
